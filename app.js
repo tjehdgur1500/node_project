74954b9fbbcdece158656a11dde3d4cf7241cfc2
tjehdgur1500 <tjehdgur1500@naver.com>
2020-08-30 22:42:12 +0900

    query string in  express router

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res, error) {
-  res.render("home");
+  var fors = "";
+  for (var i = 0; i <= 5; i++) {
+    fors = fors + `<ul><li>${i}</li></ul>`;
+  }
+  res.render("home", { title: fors });
 });
 app.get("/login", function (req, res, error) {
-  res.render("login");
+  let users = ["node js", "express", "조금더 유연한 방법의 연결"];
+  let output = `
+  <a href="/login?id=0">user1</a><br>
+  <a href="/login?id=1">user2</a><br>
+  <a href="/login?id=2">user3</a><br><br>
+  ${users[req.query.id]}
+  `;
+  res.send(output);
+  // res.render("login");
 });
 app.listen(3000, function () {
   console.log("Connected 3000 port");
