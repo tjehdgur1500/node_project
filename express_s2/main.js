a1e9dd8b087a02dc2118503f94ee85e0ac750a55
tjehdgur1500 <tjehdgur1500@naver.com>
2020-10-05 23:52:40 +0900

    express rest api

--- express_s2/main.js
+++ express_s2/main.js
@@ -36,8 +36,8 @@ app.get("/page/:pageId", (req, res) => {
         list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
+                <a href="/update/${sanitizedTitle}">update</a>
+                <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
@@ -83,6 +83,67 @@ app.post("/create", (req, res) => {
     fs.writeFile(`data/${title}`, description, "utf8", function (err) {
       res.writeHead(302, { Location: `/?id=${title}` });
       res.end();
+    });
+  });
+});
+
+app.get("/update/:pageId", (req, res) => {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = req.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(
+        title,
+        list,
+        `
+                <form action="/update_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                  <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                  </p>
+                  <p>
+                    <input type="submit">
+                  </p>
+                </form>
+                `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      );
+      res.send(html);
+    });
+  });
+});
+
+app.post("/update_process", (req, res) => {
+  var body = "";
+  req.on("data", function (data) {
+    body = body + data;
+  });
+  req.on("end", function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+        res.redirect(`/?id=${title}`);
+      });
+    });
+  });
+});
+
+app.post("/delete_process", (req, res, next) => {
+  var body = "";
+  req.on("data", function (data) {
+    body = body + data;
+  });
+  req.on("end", function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+      res.redirect("/");
     });
   });
 });
